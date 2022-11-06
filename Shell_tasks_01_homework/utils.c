b49d78624db688cc68a54971bd293524d7874ff2
Jivko <jivkon1410@gmail.com>
2022-11-06 18:59:49 +0200

    updated readme file

--- Shell_tasks_01_homework/utils.c
+++ Shell_tasks_01_homework/utils.c
@@ -163,26 +163,6 @@ char *read_line_(int fd)
 
 int count_bytes(int fd)
 {
-    // int blocks_c = 0;
-    // int bytes_c = 0;
-    // char buffer[256];
-    // int read_status = 0;
-
-    // while (sizeof buffer == (read_status = read(fd, buffer, sizeof buffer)))
-    // {
-    //     blocks_c++;
-    // }
-    // if (-1 == read_status)
-    // {
-    //     puts("Could't read file\n");
-    //     exit(1);
-    // }
-    // if (sizeof buffer != read_status)
-    // {
-    //     bytes_c += read_status;
-    // }
-    // bytes_c += sizeof buffer * blocks_c;
-
     if (-1 == lseek(fd, 0, SEEK_SET))
     {
         puts("Couldn't lseek fd\n");
